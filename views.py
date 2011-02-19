9c40c82a32d66ede8280d0da179cd522231f3dcf
Adam Playford <adam.playford@gmail.com>
2011-02-19 18:39:50 -0500

    Two bugfixes to views.py.
    Fixes:
     - render_to_response had wrong number of args. (This is a holdover from render() fix in last commit.)
     - get_object_or_404 now used to gracefully handle bad URLs in project and person views.

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
 from hackathon.models import Project, Person
@@ -13,26 +13,26 @@ def index(request):
 
 def login(request):
     varsContext = {}
-    return render_to_response(request, "hackathon/login.html", varsContext, context_instance=RequestContext(request))
+    return render_to_response("hackathon/login.html", varsContext, context_instance=RequestContext(request))
 
 def person(request, slugPerson):
     varsContext = {}
 
-    varsContext["me"] = Person.objects.get(slug=slugPerson)
+    varsContext["me"] = get_object_or_404(Person, slug=slugPerson)
     
-    return render_to_response(request, "hackathon/person.html", varsContext, context_instance=RequestContext(request))
+    return render_to_response("hackathon/person.html", varsContext, context_instance=RequestContext(request))
 
 def project(request, slugProject):
     varsContext = {}
 
-    varsContext["me"] = Project.objects.get(slug=slugProject)
+    varsContext["me"] = get_object_or_404(Project, slug=slugProject)
 
-    return render_to_response(request, "hackathon/project.html", varsContext, context_instance=RequestContext(request))
+    return render_to_response("hackathon/project.html", varsContext, context_instance=RequestContext(request))
 
 def project_sign_up(request):
     varsContext = {}
-    return render_to_response(request, "hackathon/project_sign_up.html", varsContext, context_instance=RequestContext(request))
+    return render_to_response("hackathon/project_sign_up.html", varsContext, context_instance=RequestContext(request))
 
 def signup(request):
     varsContext = {}
-    return render_to_response(request, "hackathon/signup.html", varsContext, context_instance=RequestContext(request))
+    return render_to_response("hackathon/signup.html", varsContext, context_instance=RequestContext(request))
