7df1562c613f9077c78107055ccbe6e28a758ba0
Adam Playford <adam.playford@gmail.com>
2011-02-19 18:37:31 -0500

    Bugfix in views: shortcut render() does not exist in Django 1.2. Now is render_to_response. (We'd already fixed this in index, but other functions didn't get updated.)

--- views.py
+++ views.py
@@ -13,26 +13,26 @@ def index(request):
 
 def login(request):
     varsContext = {}
-    return render(request, "hackathon/login.html", varsContext, context_instance=RequestContext(request))
+    return render_to_response(request, "hackathon/login.html", varsContext, context_instance=RequestContext(request))
 
 def person(request, slugPerson):
     varsContext = {}
 
     varsContext["me"] = Person.objects.get(slug=slugPerson)
     
-    return render(request, "hackathon/person.html", varsContext, context_instance=RequestContext(request))
+    return render_to_response(request, "hackathon/person.html", varsContext, context_instance=RequestContext(request))
 
 def project(request, slugProject):
     varsContext = {}
 
     varsContext["me"] = Project.objects.get(slug=slugProject)
 
-    return render(request, "hackathon/project.html", varsContext, context_instance=RequestContext(request))
+    return render_to_response(request, "hackathon/project.html", varsContext, context_instance=RequestContext(request))
 
 def project_sign_up(request):
     varsContext = {}
-    return render(request, "hackathon/project_sign_up.html", varsContext, context_instance=RequestContext(request))
+    return render_to_response(request, "hackathon/project_sign_up.html", varsContext, context_instance=RequestContext(request))
 
 def signup(request):
     varsContext = {}
-    return render(request, "hackathon/signup.html", varsContext, context_instance=RequestContext(request))
+    return render_to_response(request, "hackathon/signup.html", varsContext, context_instance=RequestContext(request))
