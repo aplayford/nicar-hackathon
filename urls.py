3e9bbb5621a35cb35437e5591854dab197793aea
Adam Playford <adam.playford@gmail.com>
2011-02-19 16:06:14 -0500

    URLs to go with placeholder views.

--- urls.py
+++ urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls.defaults import *
 
 
-urlpatterns = patterns('hackthon.views',
-    # (r'^$', 'index'),
+urlpatterns = patterns('hackathon.views',
+    (r'^$', 'index'),
+    (r'^login/$', 'login'),
+    (r'^person/(?P<slugPerson>[\w-])/$', 'person'),
+    (r'^project/(?P<slugProject>[\w-])/$', 'project'),
+
+    # Haven't looked at yet, but these are placeholders...
+    (r'^project_sign_up/$', 'project_sign_up'),
+    (r'^signup/$', 'signup'),
 )
