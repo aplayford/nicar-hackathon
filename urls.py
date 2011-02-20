e99d4c451958976b67e96aa806130b8533216a40
Adam Playford <adam.playford@gmail.com>
2011-02-19 23:04:03 -0500

    Convert dummy login system to working placeholder.

--- urls.py
+++ urls.py
@@ -2,11 +2,12 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('hackathon.views',
     url(r'^$', 'index', name="index"),
-    url(r'^login/$', 'login', name="login"),
     url(r'^person/(?P<slugPerson>[\w-]+)/$', 'person', name="person"),
     url(r'^project/(?P<slugProject>[\w-]+)/$', 'project', name="project"),
 
     # Haven't looked at yet, but these are placeholders...
     url(r'^project_sign_up/$', 'project_sign_up', name="project_sign_up"),
     url(r'^signup/$', 'signup', name="signup"),
+) + patterns('',
+	url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'hackathon/login.html'}),
 )
