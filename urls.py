e938fda9a6c2b54fbb48a40cba7621cdc4aa5197
Adam Playford <adam.playford@gmail.com>
2011-02-20 23:58:46 -0500

    MINOR BUGFIX: Disabled some views, but didn't kill the urls.

--- urls.py
+++ urls.py
@@ -11,10 +11,10 @@ urlpatterns = patterns('hackathon.views',
     	'project', {'edit': True}, name="edit-project"),
     
     url(r'^submit_project/$', 'submit_project', name="submit-project"),
-    url(r'^submit_project/success/$', 'submit_project_success', name="submit-project-success"),
+    #url(r'^submit_project/success/$', 'submit_project_success', name="submit-project-success"),
 
     url(r'^signup/$', 'signup', name="signup"),
-    url(r'^signup/success/$', 'signup_success', name="signup-success"),
+    #url(r'^signup/success/$', 'signup_success', name="signup-success"),
 ) + patterns('',
 	url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'hackathon/login.html'}, name="login"),
 	url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name="logout"),
